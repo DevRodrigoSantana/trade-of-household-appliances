2180b52092f4ac53e9a7cad28daa7845772232cd
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2023-03-22 01:25:24 -0300

    criado utiltarios e classe

--- Trade/src/Trucks.java
+++ Trade/src/Trucks.java
@@ -1,3 +1,30 @@
 public class Trucks {
+    private  double pesoMax;
+    private  String placa;
+    private String mototrista;
+
+
+    public Trucks(String placa, String mototrista) {
+        this.placa = placa;
+        this.mototrista = mototrista;
+        this.pesoMax = 2500;
+
+    }
+
+
+    public double getPesoMax() {
+        return pesoMax;
+    }
+
+
+    public String getPlaca() {
+        return placa;
+    }
+
+
+    public String getMototrista() {
+        return mototrista;
+    }
+    
     
 }
